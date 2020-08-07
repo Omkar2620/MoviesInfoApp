16d2954039d06e738e91ce7da087b9461f86e926
Omkar2620 <otbhostekar@gmail.com>
2020-08-07 17:04:13 +0530

    changed security for api key

--- app/src/main/java/com/example/moviesinfo/api/MoviesAPI.kt
+++ app/src/main/java/com/example/moviesinfo/api/MoviesAPI.kt
@@ -1,7 +1,7 @@
 package com.example.moviesinfo.api
 
 import com.example.moviesinfo.model.Info
-import com.example.moviesinfo.other.Constants.Companion.API_KEY
+import com.example.moviesinfo.other.Constants.Companion.MY_API_KEY
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -13,6 +13,6 @@ interface MoviesAPI {
         @Query("t")
         search: String,
         @Query("apikey")
-        apikey: String = API_KEY
+        apikey: String = MY_API_KEY
     ):Response<Info>
 }
--- app/src/main/java/com/example/moviesinfo/other/Constants.kt
+++ app/src/main/java/com/example/moviesinfo/other/Constants.kt
@@ -1,8 +1,12 @@
 package com.example.moviesinfo.other
 
+import com.example.moviesinfo.BuildConfig
+
 class Constants {
     companion object {
-        const val API_KEY = "3eb46605"
+//        REPLACE BuildConfig.API_KEY BY YOUR API KEY
+//        GET YOUR API BY SIGNING UP ON www.omdb.com
+        const val MY_API_KEY = BuildConfig.API_KEY
         const val BASE_URL = "http://www.omdbapi.com"
     }
 }
